95e05670470202824c6ba7d224807dd438459024
Olegsander78 <onkotov@gmail.com>
2022-08-29 17:05:44 +0500

    Проект создан. Добавлен генератор шума Перлина

---
+++ Assets/Scripts/TextureBuilder.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureBuilder
+{
+   public static Texture2D BuildTexture(float [,] noiseMap)
+    {
+        Color[] pixels = new Color[noiseMap.Length];
+
+        int pixelLength = noiseMap.GetLength(0);
+
+        for(int x=0;x< pixelLength; x++)
+        {
+            for(int z = 0; z < pixelLength; z++)
+            {
+                int index = (x * pixelLength) + z;
+
+                pixels[index] = Color.Lerp(Color.black, Color.white, noiseMap[x, z]);
+            }
+        }
+
+        Texture2D texture = new Texture2D(pixelLength, pixelLength);
+        texture.wrapMode = TextureWrapMode.Clamp;
+        texture.filterMode = FilterMode.Point;
+        texture.SetPixels(pixels);
+        texture.Apply();
+
+        return texture;
+    } 
+}
